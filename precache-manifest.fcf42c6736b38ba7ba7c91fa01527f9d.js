cc396c95c020656fc4af0b09600130d285987623
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2020-06-09 15:55:09 +0300

    Updates

---
+++ precache-manifest.fcf42c6736b38ba7ba7c91fa01527f9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc0d20700db3f2d0581bff5a762794",
+    "url": "/Presentation_React/index.html"
+  },
+  {
+    "revision": "69167c7c4df50cf4b3c0",
+    "url": "/Presentation_React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd7edf778554079d544a",
+    "url": "/Presentation_React/static/css/main.eeb40fee.chunk.css"
+  },
+  {
+    "revision": "69167c7c4df50cf4b3c0",
+    "url": "/Presentation_React/static/js/2.e9091a76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Presentation_React/static/js/2.e9091a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7edf778554079d544a",
+    "url": "/Presentation_React/static/js/main.40abbdb7.chunk.js"
+  },
+  {
+    "revision": "f92fa6547c56dec3e737",
+    "url": "/Presentation_React/static/js/runtime-main.7d2e8549.js"
+  },
+  {
+    "revision": "d64c8ab37f3a2bea44669601bf1d5a24",
+    "url": "/Presentation_React/static/media/fondecran.d64c8ab3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Presentation_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
